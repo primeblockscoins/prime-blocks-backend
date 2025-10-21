a37ee1b96e6b15caed0a4a0b49f033c583ecc316
Kousigaraj <kousigaraj77@gmail.com>
2025-10-21 17:45:18 +0530

    First Commit

--- controllers/cryptoController.js
+++ controllers/cryptoController.js
@@ -2,11 +2,15 @@ import fetch from "node-fetch";
 
 async function fetchCryptoData() {
   try {
-    // Using CoinGecko’s free public API
     const res = await fetch(
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false"
     );
     const data = await res.json();
+
+    if (!Array.isArray(data)) {
+      console.error("Unexpected API response:", data);
+      return [];
+    }
 
     return data.map((coin) => ({
       id: coin.id,
@@ -36,4 +40,4 @@ refreshData();
 
 export const getCryptoData = (req, res) => {
   res.json(cache);
-}
+};
